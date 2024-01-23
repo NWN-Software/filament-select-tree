2fdf936a588c9389e4cc88b2aa111773e448f747
Nicholas Martens <nicholas.martens2@gmail.com>
2024-01-23 11:44:02 +0100

    Comment wire:key='{{ rand() }}'

--- resources/views/select-tree.blade.php
+++ resources/views/select-tree.blade.php
@@ -8,20 +8,14 @@
 @endphp
 
 <x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
-    <div
-            wire:key="{{ rand() }}"
-            wire:ignore
-            x-data
-            x-load-css="[
+    <div {{-- wire:key="{{ rand() }}" --}} wire:ignore x-data
+        x-load-css="[
         @js(\Filament\Support\Facades\FilamentAsset::getStyleHref('tree', package: 'codewithdennis/filament-select-tree')),
         @js(\Filament\Support\Facades\FilamentAsset::getStyleHref('custom', package: 'codewithdennis/filament-select-tree'))
-        ]"
-    >
-        <div
-                x-ignore
-                ax-load="visible"
-                ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('tree', package: 'codewithdennis/filament-select-tree') }}"
-                x-data="tree({
+        ]">
+        <div x-ignore ax-load="visible"
+            ax-load-src="{{ \Filament\Support\Facades\FilamentAsset::getAlpineComponentSrc('tree', package: 'codewithdennis/filament-select-tree') }}"
+            x-data="tree({
                 name: '{{ $getName() }}',
                 state: $wire.{{ $applyStateBindingModifiers("\$entangle('{$getStatePath()}')") }},
                 options: {{ json_encode($getTree()) }},
@@ -41,17 +35,9 @@
                 openLevel: '{{ $getDefaultOpenLevel() }}',
                 direction: '{{ $getDirection() }}',
                 rtl: '{{ __('filament-panels::layout.direction') === 'rtl' }}'
-            })"
-        >
-            <x-filament::input.wrapper
-                    :suffix="$suffixLabel"
-                    :prefix="$prefixLabel"
-                    :prefix-icon="$prefixIcon"
-                    :suffix-icon="$suffixIcon"
-                    :disabled="$isDisabled()"
-                    :prefix-actions="$prefixActions"
-                    :suffix-actions="$suffixActions"
-            >
+            })">
+            <x-filament::input.wrapper :suffix="$suffixLabel" :prefix="$prefixLabel" :prefix-icon="$prefixIcon" :suffix-icon="$suffixIcon"
+                :disabled="$isDisabled()" :prefix-actions="$prefixActions" :suffix-actions="$suffixActions">
                 <div x-ref="tree"></div>
             </x-filament::input.wrapper>
         </div>
